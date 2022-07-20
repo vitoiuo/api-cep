d0a1763c2d9f6eb6e8db956711716a4fc30143aa
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-07-20 20:23:35 -0300

    removed useless commented lines

--- api/models.py
+++ api/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 
+# Create your models here.
+
 class Estado(models.Model):
     nome = models.CharField(max_length=128, unique=True)
     sigla = models.CharField(max_length=8, unique=True)
 
+
 class Cidade(models.Model):
     nome = models.CharField(max_length=128)
     estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
 
 class Cep(models.Model):
     cep = models.CharField(max_length=8, unique=True)
@@ -15,10 +19,3 @@ class Cep(models.Model):
     complemento = models.CharField(max_length=256, null=True, blank=True)
     bairro = models.CharField(max_length=256, null=True, blank=True)
     cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
-    # estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
-
-    # class Meta:
-        # unique_together = [['cidade_id', 'estado_id']]
-        
-
-# Create your models here.
