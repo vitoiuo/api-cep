19734f52efa24b55675ba1d77a7f4b453f93b925
Victor Magalhaes <victor.barreto@buser.com.br>
2022-07-20 14:45:41 -0300

    App models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
+from dataclasses import field
+from enum import unique
 from django.db import models
+
+
+class Estado(models.Model):
+    nome = models.CharField(max_length=128, unique=True)
+    sigla = models.CharField(max_length=8, unique=True)
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=128, unique=True)
+    estado_id = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+class Cep(models.Model):
+    cep = models.CharField(max_length=8, unique=True)
+    logradouro = models.CharField(max_length=256)
+    bairro = models.CharField(max_length=256)
+    cidade_id = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+    estado_id = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = [['cidade_id', 'estado_id']]
+        
 
 # Create your models here.
--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    'api',
+]
+
+THIRD_PARTY_APPS = [
+    'django_extensions'
+]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
