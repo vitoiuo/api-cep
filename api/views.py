34fc701f1ef59af391cb0f556d67305f4edf4481
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-07-21 12:38:41 -0300

    api complete

--- api/views.py
+++ api/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from . import models
+from .models import Estado, Cidade, Cep
 
 
 # Create your views here.
 
-def ola_mundo(request, cep):
-    cep = 1 
-    return JsonResponse(request, cep)
+def api_consulta_cep(request, cep):
+    endereco = Cep.objects.filter(cep=cep)
+
+    if not endereco:
+        resposta_consulta = {
+            'cep': 'cep inválido'
+        }
+
+    if endereco:
+        resposta_consulta = {
+            'cep': endereco[0].cep,
+            'logradouro': endereco[0].logradouro,
+            'complemento': endereco[0].complemento,
+            'bairro': endereco[0].bairro,
+            'cidade_id': endereco[0].cidade_id,
+        }
+
+    return JsonResponse(resposta_consulta)
