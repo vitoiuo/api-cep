fb0dc9f6941a05b5be02b379ffaded457f5141eb
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-07-20 21:56:33 -0300

    urls are mapped

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
 
 # Create your views here.
+
+def ola_mundo(request):
+    return HttpResponse('ola mundo', content_type='text/plain')
