69849ece69742f0890d97c7e5c6fd67ee5ac7211
Victor Magalhaes <victor.barreto@buser.com.br>
2022-07-21 13:44:47 -0300

    new gitignore addition

--- api/models.py
+++ api/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 # Create your models here.
 
-# class Estado(models.Model):
-#     nome = models.CharField(max_length=128, unique=True)
-#     sigla = models.CharField(max_length=8, unique=True)
-#
-#
-# class Cidade(models.Model):
-#     nome = models.CharField(max_length=128)
-#     estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
-#
-#
-# class Cep(models.Model):
-#     cep = models.CharField(max_length=8, unique=True)
-#     logradouro = models.CharField(max_length=256, null=True, blank=True)
-#     complemento = models.CharField(max_length=256, null=True, blank=True)
-#     bairro = models.CharField(max_length=256, null=True, blank=True)
-#     cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+class Estado(models.Model):
+    nome = models.CharField(max_length=128, unique=True)
+    sigla = models.CharField(max_length=8, unique=True)
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=128)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+
+class Cep(models.Model):
+    cep = models.CharField(max_length=8, unique=True)
+    logradouro = models.CharField(max_length=256, null=True, blank=True)
+    complemento = models.CharField(max_length=256, null=True, blank=True)
+    bairro = models.CharField(max_length=256, null=True, blank=True)
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
