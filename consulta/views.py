d9f8ea4e9a01ffa8a5bcce9640cdc9d0b7ad447b
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-07-28 11:20:33 -0300

    front end working

--- consulta/views.py
+++ consulta/views.py
@@ -9,27 +9,37 @@ def index(request):
 
 
 def consulta_por_cep(request):
+    resposta_consulta = {}
+
     cep = request.GET.get('cep')
 
-    endereco = Cep.objects.get(cep=cep)
+    if str(cep).isnumeric() and cep is not None:
+        try:
+            endereco = Cep.objects.get(cep=cep)
 
-    if endereco.logradouro and endereco.complemento is not None:
-        resposta_consulta = {
-            'cep': cep,
-            'logradouro': f'{endereco.logradouro} {endereco.complemento}',
-            'bairro': endereco.bairro,
-            'cidade': endereco.cidade.nome,
-            'estado': f'{endereco.cidade.estado.nome} ({endereco.cidade.estado.sigla})',
-        }
+            if endereco.logradouro and endereco.complemento is not None:
+                resposta_consulta = {
+                    'cep': cep,
+                    'logradouro': f'{endereco.logradouro} {endereco.complemento}',
+                    'bairro': endereco.bairro,
+                    'cidade': endereco.cidade.nome,
+                    'estado': f'{endereco.cidade.estado.nome} ({endereco.cidade.estado.sigla})',
+                }
 
-    else:
-        resposta_consulta = {
-            'cep': cep,
-            'logradouro': endereco.logradouro,
-            'complemento': endereco.complemento,
-            'bairro': endereco.bairro,
-            'cidade': endereco.cidade.nome,
-            'estado': f'{endereco.cidade.estado.nome} ({endereco.cidade.estado.sigla})',
-        }
+            else:
+                resposta_consulta = {
+                    'cep': cep,
+                    'logradouro': endereco.logradouro,
+                    'complemento': endereco.complemento,
+                    'bairro': endereco.bairro,
+                    'cidade': endereco.cidade.nome,
+                    'estado': f'{endereco.cidade.estado.nome} ({endereco.cidade.estado.sigla})',
+                }
+
+        except Cep.DoesNotExist:
+            return redirect('consulta:consulta_por_cep')
+
+    if not str(cep).isnumeric() and cep is not None:
+        return redirect('consulta:consulta_por_cep')
 
     return render(request, 'consulta/consulta_por_cep.html', {'resposta_consulta': resposta_consulta})
